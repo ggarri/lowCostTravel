8d734c69a5caaea4067c187c4f10882b39200ffd
ggarri <ggarri@gmail.com>
2014-08-04 21:28:51 +0200

    Including threads to speed up the process

---
+++ coreBundle/management/MyThread.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+import Queue
+import threading
+import time
+from datetime import date
+from coreBundle.models import *
+
+
+class consumer (threading.Thread):
+
+    def __init__(self, threadID, lock, cond, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = 'Consumer-'+str(self.threadID)
+        self.q = q
+        self.lock = lock
+        self.cond = cond
+
+    def run(self):
+        print "Starting " + self.name
+        while True:
+            self.consume()
+        print "Exiting " + self.name
+
+    def consume(self):
+        self.cond.acquire()
+        while self.q.empty():
+            print "%s: Waiting for work to do." % (self.name)
+            time.sleep(1)
+            # self.cond.wait()
+        job = self.q.get()
+        self.cond.release()
+
+        print "%s: Running flight [%s>%s] at %s" % (self.name, job['codeIn'], job['codeOut'], job['dateInFormatted'])
+        Flight.storeEdreamsFlightByCode(job['edreams_geoId'], job['edreams_geoOut']
+                                        , job['dateInFormatted'], job['dateOutFormatted'], job['tripType'])
+
+
+class producer (threading.Thread):
+
+    def __init__(self, threadID, lock, cond, q, q2):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = 'Productor-'+str(self.threadID)
+        self.q = q
+        self.q2 = q2
+        self.lock = lock
+        self.cond = cond
+
+    def run(self):
+        print "Starting " + self.name
+        while True:
+            self.produce()
+        print "Exiting " + self.name
+
+    def produce(self):
+        self.cond.acquire()
+        while self.q.empty():
+            print "%s: Waiting for work to do." % (self.name)
+            time.sleep(1)
+            # self.cond.wait()
+        job = self.q.get()
+        self.cond.release()
+
+        print "%s: Pushing [%s>%s] at %s" % (self.name, job['orig'], job['dest'], job['dateIn'])
+        self._pushFlighData(job['orig'], job['dest'], job['tripType'], job['dateIn'], job['dateOut'])
+
+    def _pushFlighData(self, orig, dest, tripType, dateIn, dateOut):
+        if len(orig) == 2:
+            countryIn = Country.objects.get(code=orig)
+            airportsIn = Airport.objects.filter(country=countryIn, is_main=True)
+        else:
+            airportsIn = Airport.objects.filter(code=orig)
+
+        for airportIn in airportsIn:
+            if len(dest) == 2:
+                airportsOut = airportIn.getBestConexionAirports(dest)
+            else:
+                airportsOut = Airport.objects.filter(code=dest)
+
+            for airportOut in airportsOut:
+                self.q2.put({
+                    'edreams_geoId': airportIn.edreams_geoId
+                    ,'edreams_geoOut': airportOut.edreams_geoId
+                    ,'dateInFormatted': dateIn
+                    ,'dateOutFormatted': None
+                    ,'tripType': tripType
+                    ,'codeIn': airportIn.code
+                    ,'codeOut': airportOut.code
+                })
--- coreBundle/management/commands/getFlights.py
+++ coreBundle/management/commands/getFlights.py
@@ -1,16 +1,28 @@
-from django.core.management.base import BaseCommand, CommandError
-from datetime import date
-from dateutil.rrule import rrule, DAILY
 import datetime
-
 import sys
-from coreBundle.models import *
+
+from django.core.management.base import BaseCommand, CommandError
+from dateutil.rrule import rrule, DAILY
+from coreBundle.management.MyThread import *
 
 class Command(BaseCommand):
 
     args = '<country_orig country_dest date_from date_to>'
-
     help = 'Closes the specified poll for voting'
+
+    consumerList = []
+    nConsumer = 10
+    consumerQueue = Queue.Queue(0)
+
+    producerList = []
+    nProducer = 2
+    producerQueue = Queue.Queue(0)
+
+    _lock = threading.Condition(threading.Lock())
+    _cond = threading.Condition(_lock)
+
+    _lock2 = threading.Condition(threading.Lock())
+    _cond2 = threading.Condition(_lock2)
 
     def handle(self, *args, **options):
 
@@ -21,9 +33,22 @@ class Command(BaseCommand):
         dateFrom = datetime.datetime.strptime(args[2], "%d/%m/%Y").date()
         dateTo = datetime.datetime.strptime(args[3], "%d/%m/%Y").date()
 
-        # self.storePeriod(args[0], args[1], dateFrom, dateTo)
-        self.storePeriod(args[1], args[0], dateFrom, dateTo)
-        self.roundTripPeriod(args[0], args[1], dateFrom, dateTo)
+        self.initThreads()
+
+        self.storePeriod(args[0], args[1], dateFrom, dateTo)
+        # self.storePeriod(args[1], args[0], dateFrom, dateTo)
+        # self.roundTripPeriod(args[0], args[1], dateFrom, dateTo)
+
+    def initThreads(self):
+        for tId in range(0, self.nConsumer):
+            t = consumer(tId, self._lock, self._cond, self.consumerQueue)
+            t.start()
+            self.consumerList.append(t)
+
+        for tId in range(0, self.nProducer):
+            t = producer(tId, self._lock2, self._cond2, self.producerQueue, self.consumerQueue)
+            t.start()
+            self.producerList.append(t)
 
     def roundTripPeriod(self, orig, dest, dateFrom, dateTo):
         aCheapestFlightGo = Flight.getListCheapestFlight(orig, dest, dateFrom, dateTo)
@@ -43,23 +68,39 @@ class Command(BaseCommand):
     def storePeriod(self, orig, dest, dateFrom, dateTo):
 
         for dt in rrule(DAILY, dtstart=dateFrom, until=dateTo):
-
-            if len(orig) == 2:
-                countryIn = Country.objects.get(code=orig)
-                airportsIn = Airport.objects.filter(country=countryIn, is_main=True)
-            else:
-                airportsIn = Airport.objects.filter(code=orig)
-
-            tripType = 'ONE_WAY'
-            dateInFormatted = dt.strftime("%d/%m/%Y")
-
-            for airportIn in airportsIn:
-                if len(dest) == 2:
-                    airportsOut = airportIn.getBestConexionAirports(dest)
-                else:
-                    airportsOut = Airport.objects.filter(code=dest)
-
-                for airportOut in airportsOut:
-                    print "From %s to %s at %s" % (airportIn.code, airportOut.code, dateInFormatted)
-                    Flight.storeEdreamsFlightByCode(airportIn.edreams_geoId, airportOut.edreams_geoId
-                                                     , dateInFormatted, None, tripType)
+            self.producerQueue.put({
+                'orig': orig
+                , 'dest': dest
+                , 'tripType': 'ONE_WAY'
+                , 'dateIn': dt.strftime("%d/%m/%Y")
+                , 'dateOut': None
+            })
+            # if len(orig) == 2:
+            #     countryIn = Country.objects.get(code=orig)
+            #     airportsIn = Airport.objects.filter(country=countryIn, is_main=True)
+            # else:
+            #     airportsIn = Airport.objects.filter(code=orig)
+            #
+            # tripType = 'ONE_WAY'
+            # dateInFormatted = dt.strftime("%d/%m/%Y")
+            #
+            # for airportIn in airportsIn:
+            #     if len(dest) == 2:
+            #         airportsOut = airportIn.getBestConexionAirports(dest)
+            #     else:
+            #         airportsOut = Airport.objects.filter(code=dest)
+            #
+            #     for airportOut in airportsOut:
+            #         self.workQueue.put({
+            #             'edreams_geoId': airportIn.edreams_geoId
+            #             ,'edreams_geoOut': airportOut.edreams_geoId
+            #             ,'dateInFormatted': dateInFormatted
+            #             ,'dateOutFormatted': None
+            #             ,'tripType': tripType
+            #             ,'codeIn': airportIn.code
+            #             ,'codeOut': airportOut.code
+            #         })
+                    # print "From %s to %s at %s" % (airportIn.code, airportOut.code, dateInFormatted)
+                    # Flight.storeEdreamsFlightByCode(airportIn.edreams_geoId, airportOut.edreams_geoId
+                    #                                  , dateInFormatted, None, tripType)
+                # self._cond.notifyAll()
