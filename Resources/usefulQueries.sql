9f32f01f9a8f03235846bf1fd1c8803f73e19101
Gabriel Garrido <gabriel.garrido@trivago.com>
2014-12-23 12:11:53 +0100

    Small fixes

--- Resources/usefulQueries.sql
+++ Resources/usefulQueries.sql
@@ -33,7 +33,7 @@ SELECT a2.code, a.code, f.*
 FROM coreBundle_country c
 INNER JOIN coreBundle_airport a ON (a.country_id = c.id)
 INNER JOIN coreBundle_flight f ON (f.edreams_geoId_in = a.edreams_geoId OR f.edreams_geoId_out = a.edreams_geoId)
-INNER JOIN coreBundle_airport a2 ON (a2.edreams_geoId = f.edreams_geoId_out OR a2.edreams_geoId = f.edreams_geoId_in)
+INNER JOIN coreBundle_airport a2 ON ((a2.edreams_geoId = f.edreams_geoId_out OR a2.edreams_geoId = f.edreams_geoId_in) AND a.id <> a2.id)
 WHERE c.code = 'CU' AND f.price <> -1
 ORDER BY f.trip_type <> 'ONE_WAY' DESC, f.price asc, f.duration_in desc
 
